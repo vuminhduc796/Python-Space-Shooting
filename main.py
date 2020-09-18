7a7a025d92fd22d16a6c5fab3261b07c98cab95e
Duc <vuminhduc796@gmail.com>
2020-09-18 11:51:50 +1000

    Initial commit for setting frame

---
+++ main.py
@@ -0,0 +1,11 @@
+import pygame
+import os
+import time
+import random
+pygame.font.init()
+
+WIDTH, HEIGHT = 1500, 750
+WIN = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("Space Shooter")
+
+
